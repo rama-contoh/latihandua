a7e798cd06f3d43f41637d56fad011fb36567ef9
rama <ramaalfi7673@gmail.com>
2023-10-01 11:09:08 +0700

    two commit

---
+++ bin/dart_2.dart
@@ -0,0 +1,10 @@
+import 'package:dart_2/dart_2.dart' as dart_2;
+
+void main(List<String> arguments) {
+  String kataPertama = "Hello";
+  String kataKedua = ", ";
+  String kataKetiga = "world!";
+
+  String gabungan = kataPertama + kataKedua + kataKetiga;
+  print(gabungan);
+}
